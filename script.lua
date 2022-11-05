d7cb1078dcc8e4530c8ed721bd98032da743635c
zyzxti#2047 <117467929+zyzxti@users.noreply.github.com>
2022-11-05 20:57:32 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -3,9 +3,9 @@ if game.PlaceId == 6284583030 or game.PlaceId == 10321372166 or game.PlaceId == 
   end  
 
 if game.PlaceId == 8726743209 then  -- Refinery Caves
-    loadstring(game:HttpGet "")()
+    loadstring(game:HttpGet "https://raw.githubusercontent.com/zyzxti/Scripts/main/Refinery%20Caves.lua")()
 end  
 
 if game.PlaceId == 10758111998 then  -- Booga Booga Reborn
-    loadstring(game:HttpGet "")()
+    loadstring(game:HttpGet "https://raw.githubusercontent.com/zyzxti/Scripts/main/Booga%20Booga.lua")()
 end  
