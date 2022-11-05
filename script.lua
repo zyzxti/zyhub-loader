4d65fabe3c88d7a88195f543716fd1caaaf1fb66
zyzxti#2047 <117467929+zyzxti@users.noreply.github.com>
2022-11-05 20:37:54 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,11 @@
+if game.PlaceId == 6284583030 or game.PlaceId == 10321372166 or game.PlaceId == "7722306047" then  -- Pet Simulator X
+    loadstring(game:HttpGet "")()
+  end  
 
+if game.PlaceId == 8726743209 then  -- Refinery Caves
+    loadstring(game:HttpGet "")()
+end  
+
+if game.PlaceId == 10758111998 then  -- Booga Booga Reborn
+    loadstring(game:HttpGet "")()
+end  
